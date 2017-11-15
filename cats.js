a3b3f4e2171ca2581acf8d34a2578c18c2b57fbb
Tony <ANCLIST@GMAIL.COM>
2017-11-15 16:12:34 -0500

    3 Cats

---
+++ cats.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var boxes = document.querySelectorAll('.cat-box');
+
+  $( '.summon-cats' ).click(function() {
+    console.log('clicked');
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'json',
+    }).done(function(data) {
+      for (var i = 0; i < data.cats.length; i++) {
+        var image = document.createElement('img');
+        image.src = data.cats[i].photo;
+        image.alt = data.cats[i].name;
+        boxes[i].append(image)
+      };
+    });
+});
+  });
